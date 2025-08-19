c1bb30bd81b8b0bd4e842156b7d3d1182c89bc81
Si Thu San <sithusanss@gmail.com>
2025-08-19 22:21:25 +0700

    build: middleware

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,32 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 
 const app = express();
 const PORT = 8080;
 
-app.use("/app", express.static("./src/app"));
+const middlewareLogResponses = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  res.on("finish", () => {
+    if (res.statusCode !== 200) {
+      console.log(
+        `[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}`
+      );
+    }
+  });
+
+  next();
+};
 
 const handlerReadindess = (req: Request, res: Response) => {
   res.set("Content-Type", "text/plain; charset=utf-8");
   res.status(200);
   res.send("OK");
 };
+
+app.use(middlewareLogResponses);
+app.use("/app", express.static("./src/app"));
 
 app.get("/healthz", handlerReadindess);
 
