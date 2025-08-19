e707b79e969c37fe40cc6ba1009f76f72650b820
Si Thu San <sithusanss@gmail.com>
2025-08-19 15:32:25 +0700

    build: health check

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
 const PORT = 8080;
 
-app.use(express.static("."));
+app.use("/app", express.static("./src/app"));
+
+const handlerReadindess = (req: Request, res: Response) => {
+  res.set("Content-Type", "text/plain; charset=utf-8");
+  res.status(200);
+  res.send("OK");
+};
+
+app.get("/healthz", handlerReadindess);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
